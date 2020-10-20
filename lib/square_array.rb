79d8a6f77f4581e9efa3b6a4579ccdf49450292d
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-20 13:31:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,14 @@ def square_array(array)
   end
   new_array
 end
+
+
+def square_array(array)
+  new_array = []
+  index = 0
+  while array[index] do
+    new_array << array[index] * array[index]
+    index += 1 
+  end
+  new_array
+end
